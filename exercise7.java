02fba0f7e053475156e21169c3810530c0d926d8
francishigs1 <55436259+francishigs1@users.noreply.github.com>
2019-09-19 00:34:27 +0100

    Add files via upload

---
+++ exercise7.java
@@ -0,0 +1,9 @@
+public class exercise7{
+
+    //main method beings the execution of Java application
+        public static void main(String[] args){
+            System.out.println("Welcome to Java Programming!");
+
+
+        }//end of main method
+}//end of class
